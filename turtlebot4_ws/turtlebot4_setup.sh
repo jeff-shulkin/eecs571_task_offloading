6d1f2903eec691be85497446c9649d5772b9e0ad
shulkinj <shulkinj@umich.edu>
2024-10-11 13:13:02 -0400

    added a couple missed ROS2 dependencies. Shouldn't fail on cd src

--- turtlebot4_ws/turtlebot4_setup.sh
+++ turtlebot4_ws/turtlebot4_setup.sh
@@ -61,7 +61,11 @@ if [ "-h" = "${1}" -o "--help" = "${1}" ]; then
 fi
 
 echo -e "Building Jazzy ROS2 TurtleBot4 Environment..."
-cd src
+if [ -d src ]; then
+	cd src
+else
+	mkdir src
+fi
 
 echo -e "Downloading iRobot Create3 Jazzy Base Packages..."
 git clone https://github.com/iRobotEducation/create3_sim.git -b jazzy
@@ -97,9 +101,33 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 # Add ROS2 repo to sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt update -y
-sudo apt install -y ros-dev-tools
-sudo apt upgrade -y
+# Install ROS and development tools sudo apt update && sudo apt install -y \
+  libbullet-dev \
+  python3-pip \
+  python3-pytest-cov \
+  ros-dev-tools
+
+# install some pip packages needed for testing
+python3 -m pip install -U \
+  argcomplete \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest
+# install Fast-RTPS dependencies
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+# install Cyclone DDS dependencies
+sudo apt install --no-install-recommends -y \
+  libcunit1-dev
 
 # Install ROS-Jazzy with GUI tools
 sudo apt install -y ros-jazzy-desktop
