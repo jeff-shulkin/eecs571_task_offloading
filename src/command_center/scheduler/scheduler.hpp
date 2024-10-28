dd6d3972059eb93117316e8d9fd35860a659c7d4
Jeffrey Shulkin <jshulkin12@gmail.com>
2024-10-27 21:48:48 -0400

    Started scheduler, currently doesn't compile. Need to add Makefile structure and fix compile errors

---
+++ src/command_center/scheduler/scheduler.hpp
@@ -0,0 +1,52 @@
+/* Author: Jeffrey Shulkin
+   Purpose: Interface for the ROS2 task scheduler. This file includes the
+   job definition, and task submission and reception interface to the
+   ROS2 action server/cient.
+*/
+
+#ifndef TASK_OFFLOAD_SCHEDULER_HPP_
+#define TASK_OFFLOAD_SCHEDULER_HPP_
+
+#include <string>
+#include <queue>
+#include <vector>
+
+// ROS2 Libraries
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+
+enum class availableJobs {
+	ROS2_DEBUG = 0,
+	NAV2_AMCL = 1,
+	NAV2_COSTMAP2D = 2
+} // availableJobs
+
+class ROS2Job {
+
+	public:
+		ROS2Job(int job_num, availableJobs curr_job, float curr_deadline):
+		num(job_num), job(curr_job), deadline_ms(curr_deadline) {}
+		void run();
+
+	private:
+		uint_32 num; // Which job am I? (debugging)
+		availableJobs job; // ROS2/Nav2 job type
+		float deadline_ms; // Job deadline in milliseconds
+
+}; // Ros2Job
+
+class taskScheduler {
+
+	public:
+		void add_job(ROS2Job j);
+		void remove_job(ROS2Job j);
+		void execute();
+		void print_all_jobs();
+
+	private:
+		std::queue<ROS2Job> job_queue;
+
+}; // TaskScheduler
+
+#endif // TASK_OFFLOAD_SCHEDULER_HPP_
+
